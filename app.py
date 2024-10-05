75f45d90eea5d3f80872eb8a5b1f72847dc31765
rcepgs2022 <119897018+rcepgs2022@users.noreply.github.com>
2024-10-04 23:35:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello from Flask on Railway!")
+    return jsonify(message="Conseguimos!!!")
 
 if __name__ == "__main__":
     app.run(debug=True)
